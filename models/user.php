ca3d1f5bb847b8275d7d7e1a018b65a6cf8a17c7
NamNN12 <namnnmb12@gmail.com>
2021-10-30 15:29:00 +0700

    update

--- models/user.php
+++ models/user.php
@@ -3,14 +3,14 @@
         private $id;
         private $email;
         private $hash_password;
-        private $full_name;
+      
 
 
-        function __construct($id, $email, $hash_password, $full_name) {
+        function __construct($id, $email, $hash_password) {
             $this->id = $id;
             $this->email = $email;
             $this->hash_password = $hash_password;
-            $this->full_name = $full_name;
+            
 
         }
 
@@ -26,9 +26,7 @@
             return $this->hash_password;
         }
 
-        public function getFullName(){
-            return $this->full_name;
-        }
+      
 
       
 
